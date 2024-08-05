20e498dcf415a3551057eaa0be8158f594fbcb00
satadhi <haldersatadhi@gmail.com>
2024-08-05 15:18:53 +0530

    enable bg carousel

--- frontend/src/pages/home/bgImages.js
+++ frontend/src/pages/home/bgImages.js
@@ -1,9 +1,9 @@
 const bgImages = [
 
-    // "https://images-eu.ssl-images-amazon.com/images/G/31/img22/vernac-t/header/Desktop_tall_Hero_Toddler-toys-Early-learning-books._CB567557312_.jpg",
+    "https://images-eu.ssl-images-amazon.com/images/G/31/img22/vernac-t/header/Desktop_tall_Hero_Toddler-toys-Early-learning-books._CB567557312_.jpg",
     "https://images-eu.ssl-images-amazon.com/images/G/31/img24/PB/July24/Gateway/BAU/Rev2/GW_PC_3000x1200_1._CB567350516_.jpg",
-    // "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Wireless/OnePlus/AVALON/Sale/D143403581_Tall_Hero_3000x1200._CB567934537_.jpg",
-    // "https://images-eu.ssl-images-amazon.com/images/G/31/img19/SiddMiniTV/3000x1200_V16._CB568211856_.jpg"
+    "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Wireless/OnePlus/AVALON/Sale/D143403581_Tall_Hero_3000x1200._CB567934537_.jpg",
+    "https://images-eu.ssl-images-amazon.com/images/G/31/img19/SiddMiniTV/3000x1200_V16._CB568211856_.jpg"
 ]
 
 
